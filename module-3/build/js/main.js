4240383b1999682221b63e49ea07224c7243daf1
Muhamad Renald <avryso@gmail.com>
2024-11-10 13:41:17 +0700

    first commit

--- module-3/build/js/main.js
+++ module-3/build/js/main.js
@@ -1,8 +1,13 @@
 "use strict";
-// type inference
-let nama = "renald putra";
-nama = "rembo";
-// type annotation
+// tipe data string
+let nama = "muhamad renald";
+// tipe data number
 let age = 24;
-console.log(nama);
-console.log(age);
+// tipe data boolean
+let isMarried = false;
+// tipe data array
+let hobbies = ["coding", "gaming", "sleeping"];
+let numbers = [1, 2, 3, 4, 5];
+// tipe data tuple
+let person = ["renald", 24, true];
+console.log(nama, age, isMarried, hobbies, numbers, person);
--- module-3/src/main.ts
+++ module-3/src/main.ts
@@ -1,9 +1,17 @@
-// type inference
-let nama = "renald putra"
-nama = "rembo"
+// tipe data string
+let nama: string = "muhamad renald";
 
-// type annotation
-let age:number = 24
+// tipe data number
+let age: number = 24
 
-console.log(nama)
-console.log(age)
+// tipe data boolean
+let isMarried: boolean = false
+
+// tipe data array
+let hobbies: string[] = ["coding", "gaming", "sleeping"] 
+let numbers: number[] = [1,2,3,4,5]
+
+// tipe data tuple
+let person: [string, number, boolean] = ["renald", 24, true]
+
+console.log(nama, age, isMarried, hobbies, numbers, person);
